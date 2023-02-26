fd1d55ccc1cbe186b236f42ea340a71980897d7d
Leroy <leroypawlick@gmail.com>
2023-02-25 17:14:30 -0700

    finished function logic on controller.js

--- server/controller.js
+++ server/controller.js
@@ -1,16 +1,38 @@
-
+let houseData = require('./db.json')
+let id = 4;
 
 module.exports = {
     getHouses: (req, res) => {
-
+        res.status(200).send(houseData);
     },
-    deleteHouses: (req, res) => {
-
+    createHouse: (req, res) => {
+       req.body.id = id;
+       console.log(req.body.id)
+       houseData.push(req.body);
+       id++;
+       res.status(200).send(houseData)
     },
-    createHouses: (req, res) => {
-
+    updateHouse: (req, res) => {
+        const { id } = req.params;
+        const { type } = req.body;
+        houseData = houseData.map((house) => {
+            if(house.id === +id && type === 'plus') {
+                house.price = +house.price + 10000
+                
+            }
+            if(house.id === +id && type === 'minus') {
+                house.price = +house.price - 10000
+            }
+            return house
+        })
+        res.status(200).send(houseData)
     },
-    updateHouses: (req, res) => {
-
+    deleteHouse: (req, res) => {
+      for(let i = 0; i < houseData.length; i++) {
+        if(houseData[i].id === +req.params.id) {
+            houseData.splice(i, 1)
+            res.status(200).send(houseData);
+        }
+      }
     }
 }
--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ app.use(cors());
 
 const houseController = require('./controller.js')
 
-
-
-
+app.get("/api/houses", houseController.getHouses)
+app.post("/api/houses", houseController.createHouse)
+app.put("/api/houses/:id", houseController.updateHouse)
+app.delete("/api/houses/:id", houseController.deleteHouse)
 
 app.listen(4004, () => console.log('Server is running on port 4004'));
